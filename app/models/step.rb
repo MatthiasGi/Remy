a76f9abbfd0eaa788df4596525f6d9ce3bce2ff2
Matthias Gianfelice <matthias@gianfelice.de>
2020-04-12 20:25:39 +0200

    Step-Model ausgearbeitet

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,2 +1,6 @@
 class Step < ApplicationRecord
+  belongs_to :recipe
+
+  validates :number, presence: true
+  validates :description, presence: true
 end
--- test/models/step_test.rb
+++ test/models/step_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class StepTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "step has to have recipe and number" do
+    s = Step.new(number: 4, description: "Test")
+    assert_not s.save
+    s = Step.new(recipe: recipes(:default), description: "Test")
+    assert_not s.save
+    s.number = 4
+    assert s.save
+  end
+
+  test "description shouldn't be blank" do
+    s = Step.new(recipe: recipes(:default), number: 4)
+    assert_not s.save
+    s.description = ""
+    assert_not s.save
+    s.description = "Test"
+    assert s.save
+  end
 end
